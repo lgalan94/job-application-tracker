a9efdf863d5334ea1c67773d75d4041917d2ebd8
Lito Galan Jr <litojrgalan@gmail.com>
2025-10-29 16:29:57 +0800

    added environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4002/api", 
+  baseURL: import.meta.env.VITE_API_URL, 
 });
 
 // ✅ Automatically attach JWT token if available
